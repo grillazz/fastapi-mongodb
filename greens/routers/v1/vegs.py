5a98c8ae2f759e4e0aacb898ae02bd12b0f4bfd3
grillazz <the@grillazz.com>
2021-09-22 20:22:43 +0200

    add helper method

--- greens/routers/v1/vegs.py
+++ greens/routers/v1/vegs.py
@@ -1,10 +1,12 @@
 
 from fastapi import APIRouter
+from fastapi.encoders import jsonable_encoder
+from starlette.status import HTTP_201_CREATED
 
 from greens import config
 from greens.routers.exceptions import NotFoundHTTPException
-from greens.schemas.vegs import DocumentResponse, ObjectIdField
-from greens.services.repository import get_document
+from greens.schemas.vegs import DocumentResponse, ObjectIdField, Document
+from greens.services.repository import retrieve_document, create_document
 
 
 global_settings = config.get_settings()
@@ -13,17 +15,29 @@ collection = global_settings.collection
 router = APIRouter()
 
 
+@router.post("/",
+             status_code=HTTP_201_CREATED,
+             response_description="Document created",
+             response_model=DocumentResponse,
+)
+async def add_document(payload: Document):
+    try:
+        payload = jsonable_encoder(payload)
+        return await create_document(payload, collection)
+    except ValueError as exception:
+        raise NotFoundHTTPException(msg=str(exception))
+
+
 @router.get(
     "/{object_id}",
     response_description="Document retrieved",
     response_model=DocumentResponse,
 )
-async def get_ro_document(object_id: ObjectIdField):
+async def get_document(object_id: ObjectIdField):
     try:
-        document = await get_document(object_id, collection)
-    except ValueError:
-        raise NotFoundHTTPException(f"No document found for {object_id=} in {collection=}")
-    return document
+        return await retrieve_document(object_id, collection)
+    except ValueError as exception:
+        raise NotFoundHTTPException(msg=str(exception))
 
 
 
