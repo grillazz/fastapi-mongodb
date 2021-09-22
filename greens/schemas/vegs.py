0a82af130750a65abe57f356659a2e6a4f14f643
grillazz <the@grillazz.com>
2021-09-22 20:22:34 +0200

    document schema update

--- greens/schemas/vegs.py
+++ greens/schemas/vegs.py
@@ -19,17 +19,22 @@ class ObjectIdField(str):
         except InvalidId:
             raise ValueError("Not a valid ObjectId")
 
+
 class Document(BaseModel):
-    id: Optional[str] = Field(alias="_id")
     name: str = Field(...)
     desc: str = Field(...)
 
-    @root_validator()
-    def validation(cls, values):
-        if values["id"] and not ObjectId.is_valid(values["id"]):
-            raise ValueError("Research Object ID is not valid.")
-        return values
+    # @root_validator()
+    # def validation(cls, values):
+    #     if values["id"] and not ObjectId.is_valid(values["id"]):
+    #         raise ValueError("Research Object ID is not valid.")
+    #     return values
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
 
 
 class DocumentResponse(Document):
-    id: str = Field(...)
+    id: ObjectIdField = Field(...)
