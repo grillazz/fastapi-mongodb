963660764f4be85cb4b375f799f580acd1ef437d
grillazz <the@grillazz.com>
2021-09-29 12:11:14 +0200

    add first unit test

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from httpx import AsyncClient
+
+from greens.config import get_settings
+from greens.main import app, init_mongo
+from greens.utils import get_logger
+
+global_settings = get_settings()
+
+
+@pytest.fixture
+async def client() -> AsyncClient:
+    async with AsyncClient(
+        app=app,
+        base_url="http://testserver",
+    ) as client:
+        app.state.logger = get_logger(__name__)
+        app.state.mongo_client, app.state.mongo_database, app.state.mongo = await init_mongo(
+            global_settings.test_db_name
+        )
+        yield client
