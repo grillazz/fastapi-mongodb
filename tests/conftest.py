340159b79de64e888edc382610135e5cd9aa3432
grillazz <the@grillazz.com>
2022-01-08 20:53:37 +0100

    anyio added for pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,15 @@ from greens.utils import get_logger
 global_settings = get_settings()
 
 
+@pytest.fixture(
+    params=[
+        pytest.param(("asyncio", {"use_uvloop": True}), id="asyncio+uvloop"),
+    ]
+)
+def anyio_backend(request):
+    return request.param
+
+
 @pytest.fixture
 async def client() -> AsyncClient:
     async with AsyncClient(
@@ -15,7 +24,9 @@ async def client() -> AsyncClient:
         base_url="http://testserver",
     ) as client:
         app.state.logger = get_logger(__name__)
-        app.state.mongo_client, app.state.mongo_database, app.state.mongo = await init_mongo(
-            global_settings.test_db_name
-        )
+        (
+            app.state.mongo_client,
+            app.state.mongo_database,
+            app.state.mongo,
+        ) = await init_mongo(global_settings.test_db_name)
         yield client
