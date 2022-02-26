40f2a4ee8fa5bcb4c33b75ded4528221fc1e2573
grillazz <the@grillazz.com>
2022-02-26 18:25:44 +0100

    add mongo check

--- greens/main.py
+++ greens/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from greens import config
 from greens.routers import router as v1
+from greens.services.repository import get_mongo_meta
 from greens.utils import get_logger, init_mongo
 
 global_settings = config.get_settings()
@@ -37,3 +38,4 @@ async def health_check():
         assert 5 / 0
     except Exception:
         app.state.logger.exception("My way or highway...")
+    return await get_mongo_meta()
