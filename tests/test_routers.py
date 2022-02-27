2a05c3e17617b7550148c6864b5326354c1ee310
kuba <tne@grillazz.com>
2022-02-27 11:43:25 +0100

    add mongo check

--- tests/test_routers.py
+++ tests/test_routers.py
@@ -4,6 +4,7 @@ from httpx import AsyncClient
 
 
 @pytest.mark.anyio
-async def test_root(client: AsyncClient):
+async def test_health_check(client: AsyncClient):
     response = await client.get("/health-check")
     assert response.status_code == status.HTTP_200_OK
+    assert response.json()["version"] == "5.0.5"
