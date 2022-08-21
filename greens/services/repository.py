2bcca27180a3ed99e8137b0371ee70dd8ba4a7c4
Jakub Miazek <the@grillazz.com>
2022-08-21 14:07:23 +0200

    bump project deps

--- greens/services/repository.py
+++ greens/services/repository.py
@@ -45,4 +45,3 @@ async def get_mongo_meta() -> dict:
         list_of_collections[db] = await greens.app.state.mongo_client[db].list_collection_names()
     mongo_meta = await greens.app.state.mongo_client.server_info()
     return {"version": mongo_meta["version"], "databases": list_databases, "collections": list_of_collections}
-
