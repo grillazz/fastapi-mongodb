b8916294add10d122e4ac13e9ce1a5745e4899ca
Elyas <elyas@Air-de-Elyas.local>
2023-07-12 13:52:53 -0300

    config: add script create db and envs

---
+++ create-db-template.sql
@@ -0,0 +1,4 @@
+-- Active: 1689131329647@@127.0.0.1@27017
+db("adjafeventos").createCollection("usuarios");
+db("adjafeventos").createCollection("organizadores");
+db("adjafeventos").createCollection("eventos");
